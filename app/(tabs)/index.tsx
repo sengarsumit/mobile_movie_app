53a4535a73b860ac1b7a19a882a3e1a5d7b5bcc1
Sumit sengar <sengarsumit45@gmail.com>
2025-05-24 10:50:15 +0530

    tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,17 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
 
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>Welcome eh </Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight: '100%', paddingBottom: 10}}>
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+        </ScrollView>
+        
 
-
-      <Link href="/movies/avengers">Avenger Movie</Link>
     </View> 
   );
 }
